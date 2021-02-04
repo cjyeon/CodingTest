4bc3b6b893ccd8e3275851c754381d5fb836799c
cjyeon <cwd6372@gmail.com>
2021-02-05 00:21:01 +0900

    [11047] 동전 0

--- BOJ/11047.cpp
+++ BOJ/11047.cpp
@@ -7,20 +7,15 @@ int main()
     cin >> n >> sum;
     int arr[n], j = n - 1;
 
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    for (int i = n - 1; sum > 0;)
-    {
-        if (sum >= arr[j])
-        {
+    for (int i = n - 1; sum > 0;) {
+        if (sum >= arr[j]) {
             result += sum / arr[j];
             sum %= arr[j];
-        }
-        else
-            j--;
+        } else j--;
     }
 
     cout << result;
