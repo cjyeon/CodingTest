f4bd87ca8896e8b175e080b03ad3a5ae37e97d62
cjyeon <cwd6372@gmail.com>
2021-02-22 23:23:32 +0900

    [2748] 피보나치 수 2

---
+++ BOJ/2748.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    long fib[91] = {0,1,};
+    cin >> n;
+
+   for (int i = 2; i <= n; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+
+    cout << fib[n] << endl;
+
+    return 0;
+}
