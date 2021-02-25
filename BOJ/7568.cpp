adb6c9e92a10577155edcea9dd857b8f95df56c2
cjyeon <cwd6372@gmail.com>
2021-02-25 10:59:49 +0900

    [7568] 덩치

---
+++ BOJ/7568.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+
+int main() {
+    int n, rank = 1;
+    pair<int, int> arr[50];
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i].first >> arr[i].second;
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i].first < arr[j].first && arr[i].second < arr[j].second) rank++;
+            
+        }
+        cout << rank << " " ;
+        rank = 1;
+    }
+    return 0;
+}
