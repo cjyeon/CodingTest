071cd31ca80f50077b51a7233f2a3083ae0af630
Jiyeon Choi <cwd6372@gmail.com>
2021-06-24 13:10:39 +0900

    [1158] 요세푸스 문제

---
+++ BOJ/1158.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    queue<int> q;
+
+    for (int i = 1; i <= n; i++)
+        q.push(i);
+
+    cout << "<";
+
+    while (n--)
+    {
+        for (int i = 0; i < k-1; i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+
+        cout << q.front();
+        q.pop();
+        
+        if (!q.empty()) cout << ", ";
+    }
+
+    cout << ">";
+
+    return 0;
+}
