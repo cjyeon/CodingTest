332c35488cc625729eced69c3b1efa91f13ab962
Jiyeon Choi <cwd6372@gmail.com>
2021-11-02 23:49:57 +0900

    타겟 넘버

---
+++ programmers/level2/타겟_넘버.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+using namespace std;
+int answer = 0;
+ 
+void dfs(vector<int> numbers, int target, int count, int sum){
+    if (count == numbers.size()) {
+        if (target == sum)        
+            answer++;
+        return;
+    }
+    dfs(numbers, target, count + 1, sum + numbers[count]);
+    dfs(numbers, target, count + 1, sum - numbers[count]);
+}
+ 
+int solution(vector<int> numbers, int target) {
+    dfs(numbers, target, 0, 0);
+
+    return answer;
+}
